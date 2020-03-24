ab371cdb2f2f530774abadd35b399922232537fc
placuszek15 <47085922+placuszek15@users.noreply.github.com>
2020-03-24 21:45:49 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from cx_Freeze import setup, Executable
+
+base = None    
+
+executables = [Executable("randommons.py", base=base)]
+
+packages = ["idna","lists","random","sys","time"]
+options = {
+    'build_exe': {    
+        'packages':packages,
+    },    
+}
+
+setup(
+    name = "RandomPokemon",
+    options = options,
+    version = "0.1b",
+    description = 'pleasework?',
+    executables = executables
+)
